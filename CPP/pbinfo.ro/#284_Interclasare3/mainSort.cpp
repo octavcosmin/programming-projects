3eb3e4ae740548462506504e1eac0a6047b9afee
octavcosmin <cosmin.tavian@gmail.com>
2021-02-14 19:08:59 +0200

    100/100

---
+++ CPP/pbinfo.ro/#284_Interclasare3/mainSort.cpp
@@ -0,0 +1,32 @@
+#include <fstream>
+#include <algorithm>
+
+using namespace std;
+ifstream fin("interclasare3.in");
+ofstream fout("interclasare3.out");
+
+int n, m, v[200005];
+
+int main() {
+    fin >> n >> m;
+    n += m;
+    for(int i = 1; i <= n;) {
+        int tmp;
+        fin >> tmp;
+        if(tmp % 2 == 0) {
+            v[i] = tmp;
+            ++i;
+        }
+        else
+            --n;
+    }
+
+    sort(v+1, v+1+n);
+
+    for(int i = 1; i <= n; ++i) {
+        fout << v[i] << ' ';
+        if(i % 20 == 0)
+            fout << '\n';
+    }
+    return 0;
+}
