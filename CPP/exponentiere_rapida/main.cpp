bbc7f6620166f57d7e100a216cd502ece4761c4d
octavcosmin <cosmin.tavian@gmail.com>
2021-05-15 10:21:53 +0300

    Not finished

---
+++ CPP/exponentiere_rapida/main.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+
+using namespace std;
+
+long long exp(int b, int e) {
+    if(e == 0) return 1;
+    if(e % 2 == 1) return (b*exp(b, e-1));
+    return exp(b, e/2)*exp(b, e/2);
+}
+
+int main() {
+    int a, b;
+    scanf("%d%d", &a, &b);
+    printf("%ld", exp(a,b));
+    return 0;
+}
